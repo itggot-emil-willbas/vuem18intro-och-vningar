abb80da05718df33b3e0dd111e2677758a4f1d26
Emil Willbas <emil@vildbas.com>
2018-10-15 20:44:50 +0200

    Lite json grejer
    Lite json grejer

---
+++ datalagringsdemo/objekt.js
@@ -0,0 +1,12 @@
+let person = {
+    name:"Emil Willbas",
+    email:"emil.willbas@fakemail.se",
+    age:29,
+    nerd:true,
+    myMethod: function(a,b){ //metoder tillåtna i js-objekt, ej i JSON
+        return a + b;
+    }
+}
+
+
+
